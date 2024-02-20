4ac5dd7b4e663c608679ef9c3e158a61828cbdf3
PedroTurik <pedroturik@gmail.com>
2024-02-19 23:08:47 -0300

    small changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 mod state;
 
-use axum::{routing::get, BoxError, Router};
+use std::{collections::HashMap, sync::atomic::{AtomicUsize, Ordering}};
+
+use axum::{extract::{Query, State}, routing::get, serve, BoxError, Router};
+use serde::Deserialize;
 use socketioxide::{
-    extract::{Data, SocketRef},
+    extract::SocketRef,
     SocketIo,
 };
 
+use state::{RoomData, ROOMS_COUNT};
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
-use tracing::{error, info, instrument};
+use tracing::{info, instrument};
+
+use crate::state::RoomStore;
+
 
 
 #[instrument]
@@ -21,14 +28,17 @@ async fn main() -> Result<(), BoxError> {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
-    let (layer, io) = SocketIo::builder().with_state(state::MessageStore::default()).build_layer();
+    let room_store = RoomStore::default();
+    let (layer, io) = SocketIo::builder().with_state(room_store.clone()).build_layer();
 
     io.ns("/", on_connect);
 
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(|| async { "Hello, world" }))
+        .route("/load_data", get(get_rooms_count))
+        .route("/room_data", get(get_room_data))
+        .with_state(room_store.clone())
         .layer(
             ServiceBuilder::new()
                 .layer(CorsLayer::permissive())
@@ -39,7 +49,23 @@ async fn main() -> Result<(), BoxError> {
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    axum::serve(listener, app).await?;
+    serve(listener, app).await?;
 
     Ok(())
+}
+
+
+async fn get_rooms_count() -> String {
+    ROOMS_COUNT.load(Ordering::Relaxed).to_string()
+}
+
+#[derive(Deserialize)]
+struct RoomDataQuery {
+    room_id: usize,
+}
+
+async fn get_room_data(Query(params): Query<RoomDataQuery>, State(state): State<RoomStore>) -> Option<RoomData> {
+    let room = state.get(params.room_id).await;
+    room
+
 }
--- src/state.rs
+++ src/state.rs
@@ -1,29 +1,45 @@
-use std::collections::{HashMap, VecDeque};
+use std::{collections::{HashMap, VecDeque}, sync::{atomic::{AtomicUsize, Ordering}, Arc}};
+use serde::Serialize;
 use tokio::sync::RwLock;
 
-#[derive(serde::Serialize, Clone, Debug)]
-pub struct Message {
-    pub text: String,
-    pub user: String,
+
+pub static ROOMS_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+
+#[derive(Serialize, Clone, Debug)]
+pub struct Player {
+    pub name: String,
+    pub score: usize,
 }
 
-pub type RoomStore = HashMap<String, VecDeque<Message>>;
-
-#[derive(Default)]
-pub struct MessageStore {
-    pub messages: RwLock<RoomStore>,
+#[derive(Serialize, Clone, Debug)]
+pub struct AnimeOpening {
+    pub title: String,
+    pub artist: String,
+    pub video_url: String,
+    pub anime_name: String,
 }
 
-impl MessageStore {
-    pub async fn insert(&self, room: &String, message: Message) {
-        let mut binding = self.messages.write().await;
-        let messages = binding.entry(room.clone()).or_default();
-        messages.push_front(message);
-        messages.truncate(20);
+#[derive(Serialize, Clone, Debug)]
+pub struct RoomData {
+    pub owner: String,
+    pub anime_opening: AnimeOpening,
+    pub users: Vec<Player>,
+}
+
+#[repr(transparent)]
+#[derive(Default, Clone, Debug)]
+pub struct RoomStore {
+    pub rooms: Arc<RwLock<HashMap<usize, RoomData>>>,
+}
+
+impl RoomStore {
+    pub async fn insert(&self, room_id: usize, room_data: RoomData) {
+        self.rooms.write().await.insert(room_id, room_data);
+        ROOMS_COUNT.fetch_add(1, Ordering::Relaxed);
     }
 
-    pub async fn get(&self, room: &String) -> Vec<Message> {
-        let messages = self.messages.read().await.get(room).cloned();
-        messages.unwrap_or_default().into_iter().rev().collect()
+    pub async fn get(&self, room: usize) -> Option<RoomData> {
+        self.rooms.read().await.get(&room).map(|v| v.clone())
     }
 }
